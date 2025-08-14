d29e3652d7988ec16d84d7dd180376b6acbe4104
Logan Sorese <logan.sorese@meetdomaine.com>
2025-08-14 00:23:38 -0400

    Fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   vite: {
     optimizeDeps: {
       exclude: ['src/js/player.js']
